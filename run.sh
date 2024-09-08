7545f75f9d64244455de8bcf8c809d5db97e3e89
buffett0323 <philip910323@gmail.com>
2024-09-08 05:30:43 +0000

    Other files

---
+++ run.sh
@@ -0,0 +1,7 @@
+python train.py train \
+    --config_path /home/buffett/research/query-bandit-buffett/config/train.yml
+
+# python train.py train \
+#     --config_path /home/buffett/research/query-bandit-buffett/config/overall.yml \
+#     --inference_only True
+
